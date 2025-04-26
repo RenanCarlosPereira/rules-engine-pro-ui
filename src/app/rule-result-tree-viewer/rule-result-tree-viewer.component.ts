757a3bf470ad81c7f6c6ed0c8be48f3d8f071bd9
Renan Pereira <rpereira@glasslewis.com>
2025-04-26 13:54:57 +0100

    add icons

--- src/app/rule-result-tree-viewer/rule-result-tree-viewer.component.ts
+++ src/app/rule-result-tree-viewer/rule-result-tree-viewer.component.ts
@@ -1,15 +1,31 @@
 import { Component, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RuleResultTree } from '../models/rule-result-tree';
-
+import {
+  AlertTriangleIcon,
+  BracketsIcon,
+  BugIcon,
+  Code2Icon,
+  ListTreeIcon,
+  LucideAngularModule,
+  TerminalIcon,
+} from 'lucide-angular';`
+`
 @Component({
   selector: 'app-rule-result-tree-viewer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule,],
   templateUrl: './rule-result-tree-viewer.component.html',
 })
 export class RuleResultTreeViewerComponent {
   @Input() resultTree!: RuleResultTree;
+
+  BugIcon = BugIcon
+  TerminalIcon = TerminalIcon
+  AlertTriangleIcon= AlertTriangleIcon
+  ListTreeIcon = ListTreeIcon
+  Code2Icon = Code2Icon
+  BracketsIcon = BracketsIcon
 
   isExpanded = signal(true);
   showInputs = signal(false); // new!
