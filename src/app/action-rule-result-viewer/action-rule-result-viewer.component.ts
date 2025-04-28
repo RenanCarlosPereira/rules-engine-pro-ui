8ccd24d36d8791e3435368ca4c04430c4bd881e7
Renan Pereira <rpereira@glasslewis.com>
2025-04-28 21:51:09 +0100

    add output

--- src/app/action-rule-result-viewer/action-rule-result-viewer.component.ts
+++ src/app/action-rule-result-viewer/action-rule-result-viewer.component.ts
@@ -2,13 +2,15 @@ import { Component, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActionRuleResult } from '../models/action-rule-result';
 import { RuleResultTreeViewerComponent } from "../rule-result-tree-viewer/rule-result-tree-viewer.component";
+import { LucideAngularModule, TerminalIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-action-rule-result-viewer',
   standalone: true,
-  imports: [CommonModule, RuleResultTreeViewerComponent],
+  imports: [CommonModule, RuleResultTreeViewerComponent, LucideAngularModule],
   templateUrl: './action-rule-result-viewer.component.html',
 })
 export class ActionRuleResultViewerComponent {
+  TerminalIcon = TerminalIcon
   @Input() actionRuleResult!: ActionRuleResult | null;
 }
